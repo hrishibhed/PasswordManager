2dd679730ca797492707156f6e168fbeeb3f2699
hrishibhed <hrishibhedhbzz@gmail.com>
2025-01-30 17:54:57 +0530

    ss

--- Dockerfile
+++ Dockerfile
@@ -19,8 +19,13 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Copy project files
 COPY . .
 
+<<<<<<< HEAD
 # Expose the port the app runs on
 EXPOSE 8000
 
 # Command to run your application
 CMD ["sh", "-c", "python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
+=======
+# Command to run your application
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+>>>>>>> ad4e2c1 (ss)
--- PM/settings.py
+++ PM/settings.py
@@ -78,41 +78,21 @@ WSGI_APPLICATION = 'PM.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-import os
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get('DATABASE_NAME'),
-#         'USER': os.environ.get('DATABASE_USER'),
-#         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-#         'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
-#         'PORT': '3306',
-#     } 
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-# #         'NAME': 'my_db',
-# #         'USER': 'hrishi',
-# #         'PASSWORD': 'hrishi1',
-# #         'HOST': 'localhost',
-# #         'PORT': '',
+        # 'NAME': 'my_db',
+        # 'USER': 'hrishi',
+        # 'PASSWORD': 'hrishi1',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
 
 
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'testdb',
-#         'USER': 'testuser',
-#         'PASSWORD': 'testpassword',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
