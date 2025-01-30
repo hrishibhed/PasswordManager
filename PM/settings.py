cf2da75359d71345667709f2e3e8016920df0c67
hrishibhed <hrishibhedhbzz@gmail.com>
2025-01-30 19:39:11 +0530

    first commint

--- PM/settings.py
+++ PM/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-import environ
-env = environ.Env()
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,7 +131,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://*.hrishibhed.tech',
-]
